4b4b22b8b6d07b01e050951a9043886bdbfc7434
Chao Yang <chao.yang@pekall.com>
2016-02-23 10:33:42 +0800

    remove unused package

--- src/com/oo/view/MoveScaleRotateView.java
+++ src/com/oo/view/MoveScaleRotateView.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Point;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AbsoluteLayout;
